9b8b169726aed351b32a31d71d4cd06aff17c9d6
ayushvermaup65-blip <ayushvermaup65@gmail.com>
2026-07-25 01:45:12 +0530

    Solved question to Print the Sum of Each Column in a 2D Array

--- src/Lecture33/_2DArrayQuestions7.java
+++ src/Lecture33/_2DArrayQuestions7.java
@@ -14,12 +14,12 @@ public class _2DArrayQuestions7 {
         int n = arr[0].length;
 
         // Traversal
-        for (int i = 0; i<m; i++){
+        for (int i = 0; i<m; i++) {
             // jaise hi main kisi nayi row me aaunga
             // vaise hi main sum=0 kardunga
 
             int sum = 0;
-            for (int j = 0; j<n; j++){
+            for (int j = 0; j < n; j++) {
                 int value = arr[i][j];
                 sum = sum + value;
             }
@@ -30,10 +30,37 @@ public class _2DArrayQuestions7 {
         return result;
     }  // T.C. -> O(m*n), S.C. -> O(n)
 
-    // Question 1: Print the Sum of Each Row in a 2D Array
+
+
+    // Question 2: Print the Sum of Each Column in a 2D Array
+    static List<Integer> colSums(int[][] arr){
+        List<Integer> result = new ArrayList<>();
+
+        int m = arr.length;
+        int n = arr[0].length;
+
+        for (int j = 0; j< n; j++){
+            int sum = 0;
+
+            for (int i = 0; i<m; i++){
+                int value = arr[i][j];
+                sum += value;
+            }
+            result.add(sum);
+        }
+        return result;
+    }  // T.C. -> O(n*m), S.C. -> O(n)
+
+
     static void main() {
+
+        // Question 1: Print the Sum of Each Row in a 2D Array
         int [][] arr = {{1, 2, 3},{4, 5, 6},{7, 8, 9}};
         System.out.println(rowSums(arr));
-    }
 
+
+        // Question 2: Print the Sum of Each Column in a 2D Array
+        int [][] arr2 = {{1, 2, 3},{4, 5, 6},{7, 8, 9}};
+        System.out.println(colSums(arr2));
+    }
 }
