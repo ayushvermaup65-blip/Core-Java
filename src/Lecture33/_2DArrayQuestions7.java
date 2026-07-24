171ae4e4cff46f6a4038252656bb69f3d8315531
ayushvermaup65-blip <ayushvermaup65@gmail.com>
2026-07-25 01:32:28 +0530

    Solved question to Print Sum of Each Row in a 2D Array

---
+++ src/Lecture33/_2DArrayQuestions7.java
@@ -0,0 +1,39 @@
+package Lecture33;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class _2DArrayQuestions7 {
+
+
+    // Question 1: Print the Sum of Each Row in a 2D Array
+    static List<Integer> rowSums(int[][] arr){
+        List<Integer> result = new ArrayList<>();
+
+        int m = arr.length;
+        int n = arr[0].length;
+
+        // Traversal
+        for (int i = 0; i<m; i++){
+            // jaise hi main kisi nayi row me aaunga
+            // vaise hi main sum=0 kardunga
+
+            int sum = 0;
+            for (int j = 0; j<n; j++){
+                int value = arr[i][j];
+                sum = sum + value;
+            }
+            // jab main saare column ki values travel and add
+            // kar chuka hounga , tab mere paas sum ki value aa jayegi
+            result.add(sum);
+        }
+        return result;
+    }  // T.C. -> O(m*n), S.C. -> O(n)
+
+    // Question 1: Print the Sum of Each Row in a 2D Array
+    static void main() {
+        int [][] arr = {{1, 2, 3},{4, 5, 6},{7, 8, 9}};
+        System.out.println(rowSums(arr));
+    }
+
+}
