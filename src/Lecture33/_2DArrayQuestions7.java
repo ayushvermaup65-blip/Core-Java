364a8480b97a191c5c8b0c646d5110043fb1f6b1
ayushvermaup65-blip <ayushvermaup65@gmail.com>
2026-07-25 23:29:50 +0530

    Solved question to Find Transpose of a Matrix

--- src/Lecture33/_2DArrayQuestions7.java
+++ src/Lecture33/_2DArrayQuestions7.java
@@ -1,6 +1,7 @@
 package Lecture33;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class _2DArrayQuestions7 {
@@ -79,6 +80,32 @@ public class _2DArrayQuestions7 {
     }  // T.C. -> O(n*m), S.C. -> O(n)
 
 
+
+    // Question 4: Find Transpose of a Matrix
+    static int[][] transpose(int[][] arr){
+        if (arr == null || arr.length == 0){
+            return new int[0][0];
+        }
+        // for original array
+        int totalRows = arr.length;
+        int totalCols = arr[0].length;
+
+        // for new array
+        int newTotalRows = totalCols;
+        int newTotalCols = totalRows;
+        int ans[][] = new int[newTotalRows][newTotalCols];
+
+        // actual logic
+
+        for (int i = 0; i<totalRows; i++){
+            for (int j = 0; j<totalCols; j++){
+                ans[j][i] = arr[i][j];
+            }
+        }
+        return ans;
+    }
+
+
     static void main() {
 
         // Question 1: Print the Sum of Each Row in a 2D Array
@@ -95,5 +122,11 @@ public class _2DArrayQuestions7 {
         // Question 3: Wave Print A Matrix
         int [][] arr3 = {{1, 2, 3},{4, 5, 6},{7, 8, 9}};
         System.out.println(wavePrintMatrix(arr3,3,3));
+
+
+
+        // Question 4: Find Transpose of a Matrix
+        int arr4[][] = {{1,2,3},{4,5,6},{7,8,9}};
+        System.out.println(Arrays.deepToString(transpose(arr4)));
     }
 }
