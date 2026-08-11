b054bc14bb913f65243b6b833d3e136d9b4dca18
Ayush Verma <ayushvermaup65@gmail.com>
2026-08-12 01:55:06 +0530

    change directory

--- src/Lecture34/_2DArrayQuestions8.java
+++ src/Lecture34/_2DArrayQuestions8.java
@@ -1,6 +1,6 @@
 package Lecture34;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class _2DArrayQuestions8 {
 
@@ -41,9 +41,72 @@ public class _2DArrayQuestions8 {
     }  // T.C. -> O(n^2), S.C. -> O(1)
 
 
+
+        // Question 2: Spiral Print of Matrix
+        static List<Integer> spiralOrder(int[][] arr){
+            int m = arr.length;
+            int n = arr[0].length;
+            List<Integer> result = new ArrayList<>();
+
+            int startingRow = 0;
+            int endingRow = m-1;
+            int startingCol = 0;
+            int endingCol = n-1;
+            while(startingRow <= endingRow && startingCol <= endingCol){
+                // row wise left to right -> startingRow print karni h, from startingCol to endingCol
+                for(int col = startingCol; col<=endingCol; col++){
+                    result.add(arr[startingRow][col]);
+                }
+                startingRow++;
+
+                // col wise top to bottom -> endingCol print karni h, from startingRow to ending row
+                for(int row = startingRow; row<=endingRow; row++){
+                    result.add(arr[row][endingCol]);
+                }
+                endingCol--;
+
+                // row wise right to left -> endingRow print karni h, from endingCol to startingCol
+                // valid endingRow present h ya nahi
+                if(startingRow <= endingRow){
+                    for(int col = endingCol; col >= startingCol; col--){
+                    result.add(arr[endingRow][col]);
+                }
+                endingRow--;
+                }
+
+
+                // col wise bottom to top -> startingCol print karni h, from endingRow to startingRow
+                // valid startingCol present h ya nahi
+                if(startingCol <= endingCol){
+                    for(int row = endingRow; row>= startingRow; row--){
+                    result.add(arr[row][startingCol]);
+                }
+                startingCol++;
+                }
+            }
+            return result;
+        }
+
+
+
     static void main() {
-        int arr[][] = {{1,2,3},{4,5,6},{7,8,9}};
+
+
+
+        // Question 1: Rotate Matrix by 90 degree
+        int arr[][] = {{1,2,3},{6,7,8},{10,11,12}};
         int ans[][] = rotateMatrix(arr,3);
         System.out.println(Arrays.deepToString(ans));
+
+
+
+
+        // Question 2: Spiral Print of Matrix
+        int arr2[][] = {{1,2,3,4},{5,6,7,8}};
+        List<Integer> result = spiralOrder(arr2);
+        for(int nums: result){
+            System.out.println(nums);
+        }
+
     }
 }
